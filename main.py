72c4c6ed9ebcc22de0bba6e2735ff660b0c77e3f
brotmensch <brotmensch@gmail.com>
2025-08-13 22:55:18 +0200

    Got an arrow

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
+    player=Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
 
     pygame.init
     clock=pygame.time.Clock()
@@ -13,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+       
         dt=clock.tick(60)/1000
         
     
