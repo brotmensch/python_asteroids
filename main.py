9ef455a819e9dbac65ea8719f73bbcf06882372c
brotmensch <brotmensch@gmail.com>
2025-08-13 23:21:07 +0200

    created groups

--- main.py
+++ main.py
@@ -4,20 +4,34 @@ from player import *
 from circleshape import *
 
 def main():
-    player=Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
+    
 
-    pygame.init
+    pygame.init()
     clock=pygame.time.Clock()
     dt=0
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    i=0
-    while i==0:
+    
+    
+    updateable=pygame.sprite.Group()
+    drawable=pygame.sprite.Group()
+    Player.containers=(updateable, drawable)
+    player=Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
+
+    
+   
+   
+   
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updateable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for drawables in drawable:
+            drawables.draw(screen)
+        
         pygame.display.flip()
        
         dt=clock.tick(60)/1000
